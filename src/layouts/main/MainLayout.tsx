497a423c1b90fb0c81419321624bd15d785d5a75
Hubert Cywka <hcywka@gmail.com>
2023-07-15 11:35:25 +0200

    TU-21: Layout changes

--- src/layouts/main/MainLayout.tsx
+++ src/layouts/main/MainLayout.tsx
@@ -2,15 +2,14 @@ import { PropsWithChildren } from 'react';
 import styles from './MainLayout.module.scss';
 import Navbar from 'components/navigation/navbar/Navbar';
 import Footer from 'components/navigation/footer/Footer';
-import GlobalLayout from '../GlobalLayout';
 
 const MainLayout = ({ children }: PropsWithChildren) => {
   return (
-    <GlobalLayout>
+    <>
       <Navbar />
       <div className={styles.content}>{children}</div>
       <Footer />
-    </GlobalLayout>
+    </>
   );
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,12 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import 'styles/reset.scss';
 import 'styles/globals.scss';
-
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { QueryClientProvider } from 'react-query';
 import { AppQueryClient } from '../services/QueryClient';
 import { SessionProvider } from 'next-auth/react';
+import GlobalLayout from '../layouts/GlobalLayout';
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -17,9 +17,11 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Team.Up</title>
         <link href="/favicon.svg" rel="icon" type="image/svg" />
       </Head>
-      <SessionProvider session={pageProps.session} basePath="/api/auth">
+      <SessionProvider session={pageProps.session}>
         <QueryClientProvider client={AppQueryClient}>
-          <Component {...pageProps} />
+          <GlobalLayout>
+            <Component {...pageProps} />
+          </GlobalLayout>
         </QueryClientProvider>
       </SessionProvider>
     </>
