30bf09f6fe940b56bf2800dc70865d370d3b4c4d
Hubert Cywka <hcywka@gmail.com>
2023-07-23 18:01:46 +0200

    TU-30: Added events list

--- src/components/events/event-item/EventItem.tsx
+++ src/components/events/event-item/EventItem.tsx
@@ -5,6 +5,7 @@ import { faClock, faPeopleGroup, faLocation, faCalendar } from '@fortawesome/fre
 import { calculateDistanceInKilometers } from '@shared/utility/LocationUtils';
 import { useUserDetailsStore } from '@stores/UserDetailsStore';
 import { memo } from 'react';
+import classNames from 'classnames';
 
 interface EventItemProps {
   event: SportEvent;
@@ -12,36 +13,67 @@ interface EventItemProps {
 
 const EventItem = ({ event }: EventItemProps) => {
   const userDetailsStore = useUserDetailsStore();
+  const startDate = new Date(event.startDate).toLocaleDateString();
+  const startTime = new Date(event.startDate).toLocaleTimeString().slice(0, -3);
+  const leftSlots = Math.floor(Math.random() * 5); // TODO remove this mock;
 
-  const getDate = () => {
-    return new Date(event.startDate).toLocaleDateString();
-  };
+  const getLastUpdated = () => {
+    if (!event.updatedAt) return;
 
-  const getTime = () => {
-    return new Date(event.startDate).toLocaleTimeString().slice(0, -3);
+    const timeDifferenceInSeconds =
+      (new Date().getTime() - new Date(event.updatedAt).getTime()) / 1000;
+
+    const minutes = Math.floor(timeDifferenceInSeconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+
+    let result = 'Last updated: ';
+
+    if (days) {
+      return result.concat(`${days} day${days === 1 ? '' : 's'} ago`);
+    } else if (hours) {
+      return result.concat(`${hours} hour${hours === 1 ? '' : 's'} ago`);
+    } else if (minutes) {
+      return result.concat(`${minutes} minute${minutes === 1 ? '' : 's'} ago`);
+    }
+
+    return result.concat('just now');
   };
 
   return (
     <article className={styles.eventItem}>
+      <p className={styles.topLine}>
+        <span className={styles.availability}>
+          <div className={classNames(styles.availabilityDot, { [styles.available]: leftSlots })} />
+          <span>
+            {leftSlots} slot{leftSlots === 1 ? '' : 's'} left
+          </span>
+        </span>
+        <span className={styles.lastUpdated}>{getLastUpdated()}</span>
+      </p>
+
+      <p className={styles.description}>{event.description}</p>
+
       <section className={styles.details}>
         <p className={styles.detailsRow}>
           <span>
-            <FontAwesomeIcon icon={faPeopleGroup} /> {event.minPlayers} - {event.maxPlayers}
+            <FontAwesomeIcon className={styles.icon} icon={faPeopleGroup} /> {event.minPlayers} -{' '}
+            {event.maxPlayers}
           </span>
           {userDetailsStore.userLocation && (
             <span>
-              <FontAwesomeIcon icon={faLocation} />{' '}
-              {calculateDistanceInKilometers(userDetailsStore.userLocation, event.location)} km
+              <FontAwesomeIcon className={styles.icon} icon={faLocation} />{' '}
+              {calculateDistanceInKilometers(userDetailsStore.userLocation, event.location)} km away
             </span>
           )}
         </p>
 
         <p className={styles.detailsRow}>
           <span>
-            <FontAwesomeIcon icon={faCalendar} /> {getDate()}
+            <FontAwesomeIcon className={styles.icon} icon={faCalendar} /> {startDate}
           </span>
           <span>
-            <FontAwesomeIcon icon={faClock} /> {getTime()}
+            <FontAwesomeIcon className={styles.icon} icon={faClock} /> {startTime}
           </span>
         </p>
       </section>
